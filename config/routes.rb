a4d033fb936d426eb71dc0639e7cea2362f85ecd
jaklahaulps <liu.perry@gmail.com>
2019-01-17 15:21:31 -0500

    ignore commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  resources :users
+  resources :sessions
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
