41b63bd06ff91c86b7ad487a17e9ebcefedeb228
jaklahaulps <liu.perry@gmail.com>
2019-01-19 22:23:00 +0000

    reviews resources route added

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,18 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions
 
-  resources :products, only: [:index, :show]
-  resources :categories, only: [:show]
+  resources :products, only: [:index, :show] do
+    resources :reviews, only: [:create, :show]
+  end 
+  
 
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
 
+  
+  resources :categories, only: [:show]
   resources :orders, only: [:create, :show]
 
   namespace :admin do
